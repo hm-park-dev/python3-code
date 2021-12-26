e762f3aa655ff36fe732073c67461295cecbe4b8
qkrgoal1111 <qkrgoal18@gmail.com>
2021-12-26 11:04:35 +0900

    Modify 6064.py

--- python_baek/bruteforce/6064.py
+++ python_baek/bruteforce/6064.py
@@ -1,26 +1,16 @@
 # 6064.py 카잉 달력
 
 def solution(M, N, x, y):
-    year = 1
-    a, b = 1, 1
-    while True:
-        if a > M:    a = 1
-        if b > N:    b = 1
-
-        # 맞을 때
-        if a == x and b == y:
-            print(year)
-            break
-        # 끝날 때
-        if a == M and b == N:
-            print(-1)
-            break
-        
-        year += 1
-        a += 1
-        b += 1
+    # (year - x) % M = 0
+    # (year - y) % N = 0
+    # 되는것을 찾아야함
+    while x <= M * N:
+        if (x - y) % N == 0:
+            return x
+        x += M
+    return -1
 
 T = int(input())
 for _ in range(T): 
     M, N, x, y = map(int, input().split())
-    solution(M, N , x, y)
+    print(solution(M, N , x, y))
