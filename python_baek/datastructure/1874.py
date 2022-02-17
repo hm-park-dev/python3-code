492c06175e2b867a99e34fb2e0c4d39dcd1d2a5c
ParkHaemi <qkrgoal18@gmail.com>
2022-02-17 14:34:34 +0900

    Add 1874.py

---
+++ python_baek/datastructure/1874.py
@@ -0,0 +1,27 @@
+# 1874.py 스택 수열
+
+# 1 -> 메모리제한 걸림?
+
+from sys import stdin
+
+n = int(stdin.readline())
+arr = [int(stdin.readline()) for _ in range(n)]
+ans = list()
+st = list()
+nums = list(range(1, n+1))
+
+for i in range(n):
+    while True: 
+        if st and st[-1] > arr[i]:
+            print('NO')
+            exit()
+        if not st or st[-1] != arr[i]:
+            st.append(nums.pop(0))
+            ans.append('+')
+        if st and st[-1] == arr[i]:
+            st.pop()
+            ans.append('-')
+            break
+
+for a in ans:
+    print(a)
