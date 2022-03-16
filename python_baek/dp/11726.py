e1f9de2f4be36d4b0f76fc94abe872aa0d910bd8
ParkHaemi <qkrgoal18@gmail.com>
2022-03-16 16:55:30 +0900

    Modify 11726.py

--- python_baek/dp/11726.py
+++ python_baek/dp/11726.py
@@ -2,14 +2,14 @@
 # https://www.acmicpc.net/problem/11726
 
 n = int(input())
-dp = [0 for _ in range(n+1)]
+dp = [0 for _ in range(1001)]
 answer = 0
 
 # 세로로 한 번
 dp[1] = 1
 dp[2] = 2
 
-for i in range(3, n+1):
+for i in range(3, 1001):
     dp[i] = dp[i-2] + dp[i-1]
 
 answer = dp[n]
