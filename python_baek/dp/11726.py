57ab783617d111a36003522609ab8211bdf73d01
ParkHaemi <qkrgoal18@gmail.com>
2022-03-16 16:49:54 +0900

    Add 11726.py

---
+++ python_baek/dp/11726.py
@@ -0,0 +1,16 @@
+# 11726.py 2×n 타일링
+# https://www.acmicpc.net/problem/11726
+
+n = int(input())
+dp = [0 for _ in range(n+1)]
+answer = 0
+
+# 세로로 한 번
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n+1):
+    dp[i] = dp[i-2] + dp[i-1]
+
+answer = dp[n]
+print(answer % 10007)
