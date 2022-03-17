6d538d7e53b1fa3c1e4753fac0c49077d4a81bb8
qkrgoal1111 <qkrgoal18@gmail.com>
2022-03-17 12:25:08 +0900

    Modify 11727.py

--- python_baek/dp/11727.py
+++ python_baek/dp/11727.py
@@ -1,2 +1,13 @@
 # 11727.py 2×n 타일링 2
 # https://www.acmicpc.net/problem/11727
+
+n = int(input())
+dp = [0 for _ in range(1001)]
+
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3, n+1):
+    dp[i] = 2 * dp[i-2] + dp[i-1]
+
+print(dp[n] % 10007)
