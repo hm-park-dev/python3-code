49ee46b9c22fca95cf92e4b03b3cc55c41a58ecb
qkrgoal1111 <qkrgoal18@gmail.com>
2022-03-24 12:38:44 +0900

    Add 14002.py

---
+++ python_baek/dp/14002.py
@@ -0,0 +1,15 @@
+# 14002.py 가장 긴 증가하는 부분 수열4
+# https://www.acmicpc.net/problem/14002
+
+# 가장 긴 증가하는 부분 수열의 길이
+# 그런 수열을 출력
+
+# Initial
+N = int(input())
+A = list(map(int, input().split()))
+answer_arr = []
+answer_len = 0
+
+# Answer
+print(answer_len)
+print(answer_arr)
