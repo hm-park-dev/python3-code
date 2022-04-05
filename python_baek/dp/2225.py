8cbc6884527c9782a613adbc4cded81ab688fb8c
hm-park-dev <hmpark042@gmail.com>
2022-04-05 13:32:44 +0900

    Add 2225.py

---
+++ python_baek/dp/2225.py
@@ -0,0 +1,13 @@
+# 2225.py 합분해
+# https://www.acmicpc.net/problem/2225
+
+import sys
+input = sys.stdin.readline
+
+# Initial
+N, K = map(int, input().split())
+mod = 1000000000
+answer = 0
+
+# Answer
+print(answer % mod)
