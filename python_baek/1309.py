99161fe03b3b81c21867012d896965b2a4959d27
hm-park-dev <hmpark042@gmail.com>
2022-04-07 13:07:49 +0900

    Add 1309.py

---
+++ python_baek/1309.py
@@ -0,0 +1,13 @@
+# 1309.py 동물원
+# https://www.acmicpc.net/problem/1309
+
+import sys
+input = sys.stdin.readline
+
+# Initial
+answer = 0
+mod = 9901
+N = int(input())
+
+# Answer
+print(answer % mod)
