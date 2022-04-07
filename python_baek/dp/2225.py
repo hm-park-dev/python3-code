bd87826d5941449bd6cecc6cd8a4190b59afa497
hm-park-dev <hmpark042@gmail.com>
2022-04-07 10:53:42 +0900

    Modify 2225.py

--- python_baek/dp/2225.py
+++ python_baek/dp/2225.py
@@ -9,5 +9,14 @@ N, K = map(int, input().split())
 mod = 1000000000
 answer = 0
 
+# Make DP Table
+# 0 을 조합해 무조건 N번째 수는 K개 이상이 나올 수 있음
+dp = [[k for _ in range(N+1)] for k in range(K+1)]
+
+for k in range(2, K + 1):
+    for i in range(2, N+1):
+        dp[k][i] = (dp[k-1][i] + dp[k][i-1]) % mod
+
 # Answer
+answer = dp[K][N]
 print(answer % mod)
