274751be4b03bf42d40ef64836884ed32621d716
hm-park-dev <hmpark042@gmail.com>
2022-04-14 19:06:57 +0900

    Modify search.py

--- python_baek/search_problem_program/search.py
+++ python_baek/search_problem_program/search.py
@@ -1,9 +1,16 @@
 # 랜덤으로 문제를 읽어오는 프로그램
-f = open(".\python_baek\search_problem_program\problem_list.txt", 'r', encoding='utf-8')
+import random
+
+f = open("C:/Users/박해미/Desktop/Project/python3-code/python_baek/search_problem_program/problem_list.txt", 'r', encoding='utf-8')
 problems = list()
 
-while True:
-    code, str = f.readline().split(',')
-    print(code, str)
-
+line = f.readline().strip()
+while line:
+    code, str = line.split(',')
+    problems.append([code, str])
+    line = f.readline().strip()
 f.close()
+
+length = len(problems)
+ret = problems[random.randrange(length)]
+print(f'이번에 풀 문제는 {ret[0]}번 {ret[1]}')
