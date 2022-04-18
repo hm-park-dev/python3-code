d398dc96bbbfabbfcb7852108347b4818622ba9f
ParkHaemi <qkrgoal18@gmail.com>
2022-04-18 20:23:38 +0900

    Solve 10972.py

--- python_baek/10972.py
+++ python_baek/10972.py
@@ -2,17 +2,22 @@
 # https://www.acmicpc.net/problem/10972
 
 # 사전순으로 다음에 오는 순열을 구하는 프로그램
-from itertools import permutations
+
+# permutations 사용시 메모리초과 등등
+
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = tuple(map(int, input().split()))
-items = [i for i in range(1, N+1)]
+arr = list(map(int, input().split()))
 
-permu = list(permutations(items, N))
-index = permu.index(arr)
-if index == len(permu) - 1:
-    print(-1)
-else:
-    print(' '.join(map(str,permu[index+1])))
+for i in range(N-1, 0, -1):
+    if arr[i-1] < arr[i]:   # 앞 열의 값이 그 뒷열의 값보다 작으면
+        for j in range(N-1, 0, -1): # 그 앞 열의 값을 맨 뒷열부터 비교
+            if arr[i-1] < arr[j]:   # 그 앞 열의 값이 뒤에 있는 열보다 작을경우
+                arr[i-1], arr[j] = arr[j], arr[i-1] # 그 두 값을 스왑함
+                arr = arr[:i] + sorted(arr[i:])
+                print(*arr)
+                exit()
+
+print(-1)
