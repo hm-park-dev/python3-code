0fe5b1e81278ef3470705d551813948f32136d19
ParkHaemi <qkrgoal18@gmail.com>
2022-11-05 12:29:44 +0900

    Add 1389.py

---
+++ python_baek/1389.py
@@ -0,0 +1,19 @@
+# 1389.py 케빈 베이컨의 6단계 법칙
+# https://www.acmicpc.net/problem/1389
+
+import sys
+input = sys.stdin.readline
+
+# Initial
+N, M = map(int, input().split())
+user = [[] for _ in range(N + 1)]
+answer = 0
+
+# Make Undirected Graph
+for _ in range(M):
+    a, b = map(int, input().split())
+    user[a].append(b)
+    user[b].append(a)
+
+# Answer
+print(answer)
